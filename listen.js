b6fdc9e64d41a990041512f5630e17249d0c45af
Amina ALI <aliyam3@yahoo.fr>
2023-12-19 14:58:38 +0100

    Mise à jour du code

--- listen.js
+++ listen.js
@@ -1,1 +1,8 @@
 // CODE HERE
+let boutton = document.querySelector("button");
+boutton.addEventListener("click", montre);
+
+function montre() {
+let paragraphe = document.querySelector("p");
+paragraphe.classList.remove("hidden");   
+}
